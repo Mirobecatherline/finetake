60d74903d08f3782799feb4118acf6176febd980
Mirobecatherline <catemirobe@gmail.com>
2025-03-15 21:20:46 +0300

    assign changes 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,18 @@ const nextConfig = {
 
     async rewrites() {
 return [
-    // {
-    //     source: '/api/:path*',
-    //     destination: 'https://photo27-2-production.up.railway.app/:path*',
-    // }
+    {
+        source: '/api/:path*',
+        destination: 'https://photo27-2-production.up.railway.app/:path*',
+    }
     // {
     //     source: '/api/:path*',
     //     destination: 'http://127.0.0.1:3000/:path*',
     // }
-    {
-        source: '/api/:path*',
-        destination: 'https://qjz9sqrm-3000.uks1.devtunnels.ms/:path*',
-    }
+    // {
+    //     source: '/api/:path*',
+    //     destination: 'https://qjz9sqrm-3000.uks1.devtunnels.ms/:path*',
+    // }
 ]        
     }
 };
